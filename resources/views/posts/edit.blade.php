6fbd17e43da80bc1aade464952036d85044fe3cf
Binod Lamsal <binodlamsalny@gmail.com>
2023-03-04 17:03:26 +0545

    This is laravel blog site with admin login

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+    <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Edit Post') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    
+                    <form action="{{ route('posts.update', $post) }}" method="post">
+                        @csrf
+                        @method('PUT')
+                        <div class="form-group">
+                            <label for="">Post Title</label>
+                            <input type="text" name="title" class="form-control" value="{{$post->title}}">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="">Post Body</label>
+                            <textarea name="body" id="" cols="30" rows="10" class="form-control">{{$post->body}}</textarea>
+                        </div>
+                        
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
